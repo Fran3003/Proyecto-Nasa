37a68df3c69b2510a1c83eb3a7589109a2c284a8
Fran3003 <franco.rantucho@gmail.com>
2023-10-19 10:06:16 -0300

    todays images, types, fetch

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "../../componets/Header";
+import fetchApi from '../../utils/fetch'
+import TodaysImage from "../../componets/TodaysImage";
+import { PostImage } from "../../types";
+import { format, sub } from "date-fns";
+
+const Home = () => {
+    const [todaysImage, setTodaysImage] = useState<PostImage>({});
+    const [lastFiveDaysImages, setLastFiveImages] = useState<PostImage[]>([])
+
+    useEffect(() =>{
+        const loadTodaysImage =async () => {
+            try {
+                const todaysImageResponse = await fetchApi();
+                setTodaysImage(todaysImageResponse);
+                
+            } catch (error) {
+                console.error(error);
+                setTodaysImage({});
+            }
+        };
+
+        const loadLast5DaysImages = async () => {
+            try {
+                const date = new Date();
+                const todaysDate = format(date, 'yyyy-MM-dd');
+                const fiveDaysAgoDate = format(sub(date, {days: 5}), 'yyyy-MM-dd')
+                
+                const lastFiveDaysImagesResponse = await fetchApi(`&start_date=${fiveDaysAgoDate}&end_date=${todaysDate}`)
+                setLastFiveImages(lastFiveDaysImagesResponse);
+                
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        loadTodaysImage().catch(null);
+        loadLast5DaysImages().catch(null);
+    }, []);
+
+    console.log(lastFiveDaysImages);
+    
+
+    return (
+    <View style={styles.container}>
+        <Header />
+        <TodaysImage {...todaysImage}/>
+    </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 16,
+        backgroundColor: 'rgba(7,26,93,255)',
+    }
+})
+
+export default Home;
